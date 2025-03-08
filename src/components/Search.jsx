d6ea21024fb595ef9024cdf7f5dc2c12322b7127
marrasluca <lmarras10@gmail.com>
2025-03-08 15:10:43 +0100

    add: Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,27 @@
+import  { useState, useEffect } from "react"
+
+
+
 export function Search(){
+
+    const[ query, setQuery ] = useState('')
+
+
+    // HandleSubmit è una funzione che viene chiamata quando il modulo viene inviato
+    const HandleSubmit = (e) => {
+        e.preventDefault()
+        console.log(query)
+    }
+
     return(
-        <h1>Search</h1>
+        <form onSubmit={e => HandleSubmit(e)}>
+
+
+            {/* gestore di eventi che viene attivato ogni volta che l'utente cambia il valore */}
+            <input type="text" onChange={ e => console.log( e.target.value ) }/> 
+
+
+            <button type="submit">Cerca</button>
+        </form>
     )
 }
